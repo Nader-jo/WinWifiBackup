30b58ea04ae7d555fbb49ce8be572fe825696a91
Nader Jemaa <nader.jomaa@yahoo.fr>
2023-11-21 22:36:05 +0100

    Create backup.cmd

---
+++ backup.cmd
@@ -0,0 +1,2 @@
+netsh wlan export profile key=clear
+powershell -Command "Select-String -Path Wi*.xml -Pattern 'keyMaterial' > Wi-Fi-PASS; Get-Content 'Wi-Fi-PASS' | ForEach-Object { \$parts = \$_ -split ':\d+:\s+|<keyMaterial>|</keyMaterial>'; if (\$parts.Length -eq 4) { [PSCustomObject]@{ name = \$parts[0].Trim().Replace('.xml', '').Replace('WiFi-', ''); password = \$parts[2].Trim() } } } | ConvertTo-Json | Set-Content 'WIFI-Pass.json'"
